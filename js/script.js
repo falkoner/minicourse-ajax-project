ca1e81c16db30121f994ec335bb208c8db5d34df
Alex Filatau <filatau@gmail.com>
2015-11-10 16:55:50 -0800

    Fetch NYT articles

--- js/script.js
+++ js/script.js
@@ -15,19 +15,43 @@ function loadData() {
 
     var street = $('#street').val();
     var city = $('#city').val();
-
+    var address = street + ", " + city;
     var size = window.innerHeight + 'x' + window.innerWidth;
 
-    console.log("Getting image for " + street + " in " + city + "with size " + size);
+    console.log("Getting image for " + street + " in " + city + " with size " + size);
 
-    var url = 'https://maps.googleapis.com/maps/api/streetview' +
-        '?location=' + street + ' ' + city +
+    var streetviewUrl = 'https://maps.googleapis.com/maps/api/streetview' +
+        '?location=' + address +
         '&size=' + size;
 
-    $('body').append('<img class="bgimg" src="' + url + '">');
+    $('body').append('<img class="bgimg" src="' + streetviewUrl + '">');
+
+    // get NY Times articles
+
+    var nytimesUrl = 'http://api.nytimes.com/svc/search/v2/articlesearch.json';
+    var nytimesParams = {'api-key': 'e5c35d5d0d22f92d8b4882018f059002:14:73426253',
+                        'q': address,
+                        'fl': 'web_url,headline,snippet',
+                        'sort': 'newest'};
+    $.getJSON(nytimesUrl, nytimesParams, function(data) {
+        console.log(data.response);
+
+        $nytHeaderElem.text('New York Times Articles About ' + address);
+
+        var items = [];
+        var link = "<li class='article'> <a href='%LINK%'>%TITLE%</a><p>%SNIPPET%</p></li>";
+        $.each(data.response.docs, function(index, article) {
+
+            items.push( link.replace('%TITLE%',article.headline.main)
+                .replace('%LINK%',article.web_url)
+                .replace('%SNIPPET%', article.snippet));
+        });
+
+        $nytElem.append(items.join(""));
+    });
 
     return false;
-};
+}
 
 $('#form-container').submit(loadData);
 
