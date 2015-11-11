9fea0339535f21bb6bdc34eee485eb0fa0ac634e
Alex Filatau <filatau@gmail.com>
2015-11-11 11:56:35 -0800

    Fetch Wikipedia articles; note timeout funciton as failsafe

--- js/script.js
+++ js/script.js
@@ -33,22 +33,68 @@ function loadData() {
                         'q': address,
                         'fl': 'web_url,headline,snippet',
                         'sort': 'newest'};
-    $.getJSON(nytimesUrl, nytimesParams, function(data) {
-        console.log(data.response);
 
-        $nytHeaderElem.text('New York Times Articles About ' + address);
+    $.getJSON(nytimesUrl, nytimesParams)
+        .done(function(data) {
+            console.log('Response from NYT is successful:');
+            console.log(data);
 
-        var items = [];
-        var link = "<li class='article'> <a href='%LINK%'>%TITLE%</a><p>%SNIPPET%</p></li>";
-        $.each(data.response.docs, function(index, article) {
+            $nytHeaderElem.text('New York Times Articles About ' + address);
 
-            items.push( link.replace('%TITLE%',article.headline.main)
-                .replace('%LINK%',article.web_url)
-                .replace('%SNIPPET%', article.snippet));
+            var items = [];
+            var link = "<li class='article'> <a href='%LINK%'>%TITLE%</a><p>%SNIPPET%</p></li>";
+
+            $.each(data.response.docs, function(index, article) {
+                items.push( link.replace('%TITLE%',article.headline.main)
+                    .replace('%LINK%',article.web_url)
+                    .replace('%SNIPPET%', article.snippet));
+            });
+
+            $nytElem.append(items.join(""));
+        })
+        .fail(function(jqxhr, textStatus, error){
+            var err = textStatus + ", " + error;
+            console.log("Request from NYT Failed: " + err );
+            $nytHeaderElem.text('New York Times Articles About ' + address + ' Can\'t be loaded');
         });
 
-        $nytElem.append(items.join(""));
-    });
+    // get wikipedia articles
+    var wikiUrl = 'https://en.wikipedia.org/w/api.php';
+    var wikiParams = {dataType: 'jsonp',
+                    data: {action: 'query',
+                            list: 'search',
+                            srsearch: address,
+                            format: 'json'}};
+
+    var wikiRequestTimeout = setTimeout(function() {
+        $wikiElem.text('Failed to get Wikipedia resources');
+    }, 8000);
+
+    $.ajax(wikiUrl, wikiParams)
+        .done(function(data) {
+            console.log('Response from Wikipedia is successful:');
+            console.log(data);
+
+            var items = [];
+            var link = "<li class='article'>"+
+                " <a href='https://en.wikipedia.org/wiki/%LINK%'>%TITLE%</a>"+
+                "<p>%SNIPPET%</p></li>";
+
+            $.each(data.query.search, function(index, article) {
+                items.push( link.replace('%TITLE%',article.title)
+                    .replace('%LINK%',article.title)
+                    .replace('%SNIPPET%', article.snippet));
+            });
+
+            $wikiElem.append(items.join(""));
+
+            clearTimeout(wikiRequestTimeout);
+        })
+        .fail(function(erjqxhr, textStatus, error) {
+            var err = textStatus + ", " + error;
+            console.log("Request from Wikipedia Failed: " + err );
+        });
+
 
     return false;
 }
